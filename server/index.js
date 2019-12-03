44abd666c4f859a3093a2d9b159a10ecc3f763b6
pachecoSen <pachecodr2013@gmail.com>
2019-12-02 19:05:47 -0600

    Multi procesos

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const { resolve } = require('path'),
-    {green:success} = require( 'terminal-kit' ).terminal;
+    {green:success, red:error} = require( 'terminal-kit' ).terminal,
+    cluster = require('cluster'),
+	os = require('os');
 
 const path = require(resolve(__dirname, '../server/config/path'));
 
@@ -9,10 +11,21 @@ const {port} = require(path('config')),
     server = require(path('server')),
     socket = require(path('socket'));
 
-server.listen(port, (err) => {
-    if (err) throw new Error(err);
+if(cluster.isMaster){
+    for(let c = 0; c < os.cpus().length; c++)
+        cluster.fork();
+}
+else{
+    server.listen(port, (err) => {
+        if (err) throw new Error(err);
+    
+        success(`\n\n\n\n\nServer started on port: ${ port }\n`);
+        success(`Iniciando Socket...`);
+        socket(server);
+    });
+}
 
-    success(`\n\n\n\n\nServer started on port: ${ port }\n`);
-    success(`Iniciando Socket...`);
-    socket(server);
+cluster.on('exit', worker => {
+	error(`Proceso ${worker.id}, Finalizo`);
+	worker.fork();
 });
